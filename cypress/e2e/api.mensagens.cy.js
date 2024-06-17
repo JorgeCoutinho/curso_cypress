d16dbfff7a390abaa87fc9e3a0b0c1112df28b09
JORGE MIKAEL COUTINHO ALVES <jmca.lic19@uea.edu.br>
2024-06-17 14:28:38 -0300

    finalizando

--- cypress/e2e/api.mensagens.cy.js
+++ cypress/e2e/api.mensagens.cy.js
@@ -1,14 +1,13 @@
 // Teste com probelemas
 describe('Api Adopet', () => {
-    const authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMTY0M2NkNi03MTEyLTQxNWItOTVkMi0wNzkwNGIwZDFhMWMiLCJhZG9wdGVyTmFtZSI6IkFuYSBkZSBKZXN1cyIsImlhdCI6MTcwOTA0MTMwNSwiZXhwIjoxNzA5MzAwNTA1fQ.nhiaUyKdvN8RVxCkvne2gyI5n_pYnY_OMMdogMMqOlc`
-
+    
     //Atenção, o token possui um prazo de validade. Dessa forma, você deverá realizar o login e copiar o toker presente no Local Storage do seu navegador.
 
     it('Mensagens da API', () => {
         cy.request({
             method: 'GET',
             url: 'https://adopet-tau.vercel.app/static/css/main.26ceb43b.css',
-            headers: { authorization }
+            headers: Cypress.env()
 
         }).then((res) => {
             expect(res.status).to.be.equal(200)
--- cypress/e2e/login-incorreto.cy.js
+++ cypress/e2e/login-incorreto.cy.js
@@ -1,10 +1,9 @@
-// criando um duble para a requisição POST
 describe('Página de login', () => {
     beforeEach(() => {
         cy.visit('https://adopet-frontend-cypress.vercel.app');
         cy.get('[data-test="login-button"]').click();
-        cy.intercept('POST', 'https://adopet-api-i8qu.onrender.com/adotante/login', {//intercepta a requisição POST
-            statusCode: 400,//retorna status code 400
+        cy.intercept('POST', 'https://adopet-api-i8qu.onrender.com/adotante/login', {
+            statusCode: 400,
         }).as('stubPost')
     })
 
@@ -21,19 +20,3 @@ describe('Página de login', () => {
     })
 
 })
-
-
-// describe('Página de login', () => {
-//     beforeEach(() => {
-//         cy.visit('https://adopet-frontend-cypress.vercel.app/');
-//         cy.get('[data-test="login-button"]').click();
-//     })
-//     it('Deve preencher os campos do login incorretamente a e exibir mensagens ao usuário', () => {
-//         cy.get('[data-test="input-loginEmail"]').type('ana@emai');
-//         cy.get('[data-test="input-loginPassword"]').type('Senha123');
-//         cy.contains('Por favor, verifique o email digitado').should('be.visible');
-//         cy.contains('Esqueci minha senha').should('be.visible');
-//     })
-// })
-
-
